7a06f057cb2ec378925cd5363af064ae81d82d7d
Anthony Romrell <anthony@artisandream.com>
2018-03-15 09:31:58 -0600

    Updates to Store

---
+++ PlayerStoreDataBase.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class PlayerStoreDataBase : ScriptableObject
+{
+
+	public List<PurchaseableObjectBase> PurchasedList;
+
+
+}
---
+++ StoreBase.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class StoreBase : ScriptableObject
+{
+
+	public PlayerStoreDataBase Purchases;
+
+	public abstract void MakePurchase();
+
+
+}
